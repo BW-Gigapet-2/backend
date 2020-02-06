2101222ab8e4737b9dbf8392707d7ce179fd7bfa
BenSolt <52627840+BenSolt@users.noreply.github.com>
2020-02-06 16:17:40 -0500

    added production to kenxfile

--- knexfile.js
+++ knexfile.js
@@ -31,5 +31,18 @@ module.exports = {
     seeds: {
       directory: "./database/seeds"
     }
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations:{
+      directory: "./database/migrations"
+    },
+    seeds: {
+      directory: "./database/seeds/production"
+    },
+    pool: { min: 2, max: 10 },
+    useNullAsDefault: true
   }
+
 };
